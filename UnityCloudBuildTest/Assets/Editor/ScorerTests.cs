17c2a25efad82ac3bb415ff1a2f6dec8e525c8d8
Sherwin Espela <sherwin.espela@gmail.com>
2021-01-04 16:52:44 -0800

    initial commit

---
+++ UnityCloudBuildTest/Assets/Editor/ScorerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+public class ScorerTests
+{
+    [Test]
+    public void ComputeBasicScoreSuccess_Test()
+    {
+        // 1. ARRANGE
+        var scorer = new Scorer();
+        int normalScore = 100;
+        int bonusScore = 200;
+        int expectedScore = normalScore + bonusScore;
+
+        // 2. ACT
+        int score = scorer.ComputeBasicScore(normalScore, bonusScore);
+
+        // 3. ASSERT
+        Assert.That(score, Is.EqualTo(expectedScore));
+    }
+
+    [Test]
+    public void ComputeBasicScoreFail_Test()
+    {
+        // 1. ARRANGE
+        var scorer = new Scorer();
+        int normalScore = 100;
+        int bonusScore = 200;
+        int expectedScore = 80 + 90;
+
+        // 2. ACT
+        int score = scorer.ComputeBasicScore(normalScore, bonusScore);
+
+        // 3. ASSERT
+        Assert.That(score, Is.EqualTo(expectedScore));
+    }
+}
---
+++ UnityCloudBuildTest/Assets/Scripts/Scorer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Scorer
+{
+    public int ComputeBasicScore(int normalScore, int bonusScore)
+	{
+		int score = normalScore + bonusScore;
+		return score;
+	}
+}
